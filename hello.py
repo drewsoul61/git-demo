e7287b9b345aad6894839cc3f2557b52be561523
Drew <drew.soul61@thehutgroup.com>
2022-09-29 13:14:43 +0100

    Added Jupiter feature

--- hello.py
+++ hello.py
@@ -1,5 +1,16 @@
+import sys
+
 
 def hello():
+	if sys.argv[1] == "Jupiter":
+		hellojupiter()
+	else:
+		helloworld()
+
+def hellojupiter():
+	print("Hello, Jupiter")
+
+def helloworld():
 	print("Hello, World")
 
 if __name__ == '__main__':
